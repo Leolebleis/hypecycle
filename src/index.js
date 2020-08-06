9612ce6f93c3aba05dcac248a85f0f85075ae45f
leo <lelebleis@deloitte.co.uk>
2020-08-06 19:30:16 +0200

    correcting import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Analytics from 'analytics'
-import simpleAnalyticsPlugin from '@analytics/simple-analytics'
+import simpleAnalyticsPlugin from 'analytics-plugin-simple-analytics'
 
 const analytics = Analytics({
   app: 'hypecycle',
